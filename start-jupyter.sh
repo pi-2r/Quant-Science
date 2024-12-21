9e1c6f939d770fdb313c0ad66a26feadadfd3262
pierre <pierre.theroode@gmail.com>
2024-12-21 11:20:19 -0500

    Setting default dark theme

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 source activate quant-stack
+
+# Creation of the directory for JupyterLab settings
+mkdir -p /root/.jupyter/lab/user-settings/@jupyterlab/apputils-extension
+
+# Setting default dark theme
+cat << EOF > /root/.jupyter/lab/user-settings/@jupyterlab/apputils-extension/themes.jupyterlab-settings
+{
+    "theme": "JupyterLab Dark"
+}
+EOF
+
 jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.password="$JUPYTER_PASSWORD"
